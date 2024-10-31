bc7b65b571ab94d5662075fe577585c5f2ce6822
tspu_01 <tspu_01@mail.ru>
2024-10-31 15:48:01 +0700

    data types

---
+++ main.py
@@ -0,0 +1,27 @@
+# integer
+number_1 = 12
+
+# float
+number_2 = 15.15 # float
+
+print(
+    number_1,
+    number_2,
+)
+
+string_1 = 'hello "world"'
+string_2 = "hello 'world'"
+string_3 = "hello \\ \"world\" \\"
+string_4 = "\n"
+
+print(
+    string_1, 
+    string_4,
+    string_2,
+    string_4,
+    string_3,
+)
+
+
+
+
