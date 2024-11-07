61736ea982123c29a8285193c95859f5bdfe05cb
tspu_01 <tspu_01@mail.ru>
2024-11-07 15:57:58 +0700

    add new datatypes

---
+++ arr.py
@@ -0,0 +1,44 @@
+arr_1 = [1, 23, 561, 5, 6]
+
+# print(
+#     arr_1,
+#     arr_1[0], # first element
+#     arr_1[-1], # last element
+#     len(arr_1), # count elements
+# )
+
+# if len(arr_1) > 0:
+#     print(
+#         arr_1[len(arr_1) - 1],
+#     )
+# else:
+#     print('Empty')
+
+arr_2 = [
+    arr_1, arr_1, arr_1, arr_1,
+]
+
+# arr_2.append(88)
+
+# del arr_2[0]
+
+arr_2[-1].append(123)
+
+print(arr_1)
+
+# arr_2.remove(12)
+# arr_2.remove(arr_1)
+
+print(arr_2)
+
+# for a in arr_2:
+#     print(id(a))
+
+#     print(
+#         f'{a} has type {type(a)}',
+#     )
+
+print('arr_1', arr_1)
+
+print(id(arr_1))
+
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ print(
 
 string_1 = 'hello "world"'
 string_2 = "hello 'world'"
-string_3 = "hello \\ \"world\" \\"
+string_3 = "hello \\ \"world\" \\" 
 string_4 = "\n"
 
 print(
@@ -22,6 +22,28 @@ print(
     string_3,
 )
 
+string_5 = string_1.title()
+string_6 = "string string STRING string string".title()
 
+string_7 = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lacus lacus, malesuada id tempor et, consequat vitae dui. Fusce quis placerat metus. Curabitur vel arcu mi. Cras euismod dolor urna, nec ultrices ex vulputate eu. Cras tincidunt nibh at dui porttitor gravida. Integer urna purus, efficitur ac metus in, convallis rutrum ante. Fusce eu elit facilisis risus tempor imperdiet sit amet sed tortor. Curabitur ut velit nec lorem consequat laoreet eu eget mi. Nam eleifend id nibh quis dapibus. Sed rutrum, tellus et porttitor suscipit, mauris ligula luctus augue, vel pharetra ligula felis at turpis. Vivamus faucibus viverra orci a dapibus. Proin eget magna in eros tempor ultricies sit amet sed turpis.'
+string_8 = 'Sed magna libero, iaculis sit amet erat ornare, lacinia placerat ante. Vestibulum semper risus turpis, sed sodales arcu iaculis sit amet. Maecenas iaculis diam ut tortor efficitur condimentum. Curabitur vitae nisi ligula. Sed varius iaculis suscipit. Proin quis nulla quam. In sed sagittis neque. Nulla metus lorem, euismod quis tellus nec, facilisis venenatis erat. Duis condimentum, ex vestibulum malesuada cursus, mi lectus blandit sem, eu ullamcorper massa purus ac massa. Ut sed justo non mauris fringilla porttitor. Sed sit amet convallis sapien.'
+string_9 = string_7 + string_8.upper()
+
+string_10 = f'{string_1}, asdasd, asd, {string_6}'
+
+print(
+    string_5,
+    string_6,
+    string_4,
+    string_6.lower(),
+    string_4,
+    string_6.upper(),
+)
+
+print(
+    string_9,
+    string_4,
+    string_10,
+)
 
 
